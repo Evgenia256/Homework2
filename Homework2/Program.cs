be0c46105aa46e8af67fb338638dd3203d41fbd3
nikita starostin <n.starostin@itransition.com>
2023-10-22 21:40:07 +0300

    add meaningfull names

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,40 +1,40 @@
 ﻿Console.WriteLine("Напишите число №1:");
-var a =Convert.ToInt32(Console.ReadLine());
+var firstNumber =Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Напишите число №2:");
-var b =Convert.ToInt32(Console.ReadLine());
+var secondNumber =Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Напишите знак операции * или ^ или % или + или - :");
-var c = Convert.ToChar(Console.ReadLine());
-if (c == '+')
+var operationCharacter = Convert.ToChar(Console.ReadLine());
+if (operationCharacter == '+')
 {
-    var sum = a + b;
+    var sum = firstNumber + secondNumber;
     Console.WriteLine("число №1 + число №2 ="+" "+sum ); 
 }
 else
 {
     Console.WriteLine();
 }
-if(c=='*')
+if(operationCharacter=='*')
 {
-    var Mult = a * b;
+    var Mult = firstNumber * secondNumber;
     Console.WriteLine("число №1 * число №2 ="+" "+Mult);
 }
 else
 {
     Console.WriteLine(); 
 }
-if (c == '-')
+if (operationCharacter == '-')
 {
-    var Minus = a - b;
+    var Minus = firstNumber - secondNumber;
     Console.WriteLine("число №1 - число №2 ="+" "+Minus); 
 }
 else
 {
     Console.WriteLine();
 }
-if (c == '%')
+if (operationCharacter == '%')
 {
-    var RemainderDivision = a % b;
+    var RemainderDivision = firstNumber % secondNumber;
     Console.WriteLine("число №1 % число №2 ="+" "+RemainderDivision);
 }
 else
