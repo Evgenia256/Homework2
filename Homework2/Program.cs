fe9d7b36dc3a113d69db177f381efad81064b988
nikita starostin <n.starostin@itransition.com>
2023-10-22 22:27:45 +0300

    add sample of comments

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,4 +1,12 @@
 ﻿Console.Write("Enter number 1: ");
+// Console - this a class
+// . - this is an operator that allows to access members of a class
+// Write - is a method, it is a member of class Console
+// () - operator that allows to pass arguments to a method and make
+// "Enter number 1" - is a string literal
+// ; - is a statement terminator, means end of statement in the C#
+// In this line we are calling Write method of Console class and passing a string literal as an argument
+
 var firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter number 2: ");
 var secondNumber = Convert.ToInt32(Console.ReadLine());
