2d2d7ed36bdbf7ff89ec7d111c72579be44c3c3b
nikita starostin <n.starostin@itransition.com>
2023-10-22 21:44:58 +0300

    refactor code to shorter version with switch expressions

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -5,39 +5,18 @@ var secondNumber =Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Напишите знак операции * или ^ или % или + или - :");
 var operationCharacter = Convert.ToChar(Console.ReadLine());
-if (operationCharacter == '+')
+int? result = operationCharacter switch
 {
-    var sum = firstNumber + secondNumber;
-    Console.WriteLine("число №1 + число №2 ="+" "+sum ); 
-}
-else
-{
-    Console.WriteLine();
-}
-if(operationCharacter=='*')
-{
-    var Mult = firstNumber * secondNumber;
-    Console.WriteLine("число №1 * число №2 ="+" "+Mult);
-}
-else
-{
-    Console.WriteLine(); 
-}
-if (operationCharacter == '-')
-{
-    var Minus = firstNumber - secondNumber;
-    Console.WriteLine("число №1 - число №2 ="+" "+Minus); 
-}
-else
-{
-    Console.WriteLine();
-}
-if (operationCharacter == '%')
-{
-    var RemainderDivision = firstNumber % secondNumber;
-    Console.WriteLine("число №1 % число №2 ="+" "+RemainderDivision);
-}
-else
-{
-    Console.WriteLine();
-}
+    '*' => firstNumber * secondNumber,
+    '/' => firstNumber / secondNumber,
+    '+' => firstNumber + secondNumber,
+    '-' => firstNumber - secondNumber,
+    '%' => firstNumber % secondNumber,
+    _ => null
+};
+
+var resultMessage = result == null
+    ? $"Извините, операция {operationCharacter} не поддерживается"
+    : $"Число 1 ({firstNumber}) {operationCharacter} Число 2 ({secondNumber}) = {result}";
+
+Console.WriteLine(resultMessage);
